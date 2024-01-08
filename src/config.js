6bd32f6c420c365bdb3ea8339b408fb59c7aee73
Guillermodiazvg5 <guillermodiazvg5@hotmail.com>
2024-01-07 23:52:35 -0500

    guardado7

--- src/config.js
+++ src/config.js
@@ -5,11 +5,11 @@ module.exports = {
 
     db: {
 
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "Gddv19922@",
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432 ,
+    database: process.env.DB_DATABASE || "healthfoods"
 }
 
 }
