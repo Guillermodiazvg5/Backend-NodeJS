bc95a73518d642a46d15fcc1c333cdd35461ed45
Guillermodiazvg5 <guillermodiazvg5@hotmail.com>
2024-01-30 16:14:21 -0500

    commit3.30012024

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -93,11 +93,13 @@ const CreateOrder = async (req, res) => {
       items: NuevoArray,
 
       back_urls: {
+
         success: "https://render-frontend-react-healthfoods.onrender.com/",
 
-        
+
 
         failure: "https://render-frontend-react-healthfoods.onrender.com/",
+     
 
       },
 
--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const {Pool} = require('pg')
 const {db} = require('./config')
 
 
-/*
+
 
 const pool = new Pool({
 
@@ -10,25 +10,9 @@ const pool = new Pool({
     password: db.password,
     host: db.host,
     port: db.port,
-    database: db.database
-
-
-})
-
-*/
-
-const pool = new Pool({
-
-    user: 'databasepostgres',
-    password: 'ThxCrD8zwBlyDlYqnPiXEpkGUyLfG5CU' ,
-    host: 'dpg-cmdmadmd3nmc73dl63v0-a.oregon-postgres.render.com',
-    port: 5432,
-    database: 'healthfoods' ,
+    database: db.database,
     ssl: true
-
     
-
 })
-
 
 module.exports= pool;
